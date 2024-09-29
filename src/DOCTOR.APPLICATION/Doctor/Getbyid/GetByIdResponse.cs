d8ec62443c817e001b3734c07a4feac1af3c7690
Matheus <matheusmenezesribeiro@hotmail.com>
2024-09-29 17:46:46 -0300

    add getbyid

---
+++ src/DOCTOR.APPLICATION/Doctor/Getbyid/GetByIdResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace DOCTOR.APPLICATION.Doctor.Getbyid;
+
+public class GetByIdResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+
+    public static GetByIdResponse ToResponse(DOMAIN.Doctor doctor)
+        => new GetByIdResponse
+        {
+            Email = doctor.Email,
+            Id = doctor.Id,
+            Name = doctor.Name,
+            Phone = "123"
+        };
+
+}
